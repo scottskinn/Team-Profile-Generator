148c4dce682df46dceaff8d36cc510fe69cc662f
scottskinn <84105761+scottskinn@users.noreply.github.com>
2021-07-31 12:46:16 -0400

    class properties

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const path = require('path');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
+// WHEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// use inquirer to get answer 
+// create new manager instance
+// populate new manager instance with inquirer answer
+const manager = new Manager()
+
+const teamDataArgs = process.argv.slice(2, process.argv.length);
+console.log(teamDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
